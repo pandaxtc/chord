da0ba75367f84bf6beed61b5e4bdd179bef28602
krashanoff <lkrashanoff2@gmail.com>
2020-09-23 00:18:00 -0700

    Setup Discord, GUI interprocess communication.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,62 @@
-use std::env;
-use std::sync::mpsc;
-use serenity::async_trait;
-use serenity::prelude;
-use serenity::client::{Client, Context, EventHandler};
-use serenity::model::{
-    gateway::Ready, channel::Message
+use ansi_term::Color;
+use iced::{executor, Application, Column, Command, Container, Element, Length, Settings, Text};
+use serenity::{
+    async_trait,
+    client::{Client, Context, EventHandler},
+    framework::standard::{macros::group, StandardFramework},
+    model::{channel::Message, gateway::Ready},
+    prelude,
 };
-use serenity::framework::standard::{
-    StandardFramework,
-    macros::{
-        group
+use std::{env, process::exit, sync::mpsc, thread, time::Duration};
+use tokio::runtime::Runtime;
+
+fn main() {
+    let (tx, rx): (mpsc::Sender<DiscordEvent>, mpsc::Receiver<DiscordEvent>) = mpsc::channel();
+
+    // Spawn Discord
+    let _discord = thread::spawn(move || {
+        let mut toki = Runtime::new().unwrap();
+
+        toki.block_on(async move {
+            let framework = StandardFramework::new().group(&GENERAL_GROUP);
+
+            // Login with a bot token from the environment
+            let token = env::var("DISCORD_TOKEN").expect("Discord token is not set!");
+            let mut client = Client::new(token)
+                .event_handler(Handler)
+                .framework(framework)
+                .await
+                .expect("Error creating client");
+
+            // Pass down send channel clone to client data.
+            let discord_tx = tx.clone();
+            {
+                let mut data = client.data.write().await;
+                data.insert::<DiscordPipe>(std::sync::Mutex::new(discord_tx));
+            }
+
+            if let Err(e) = tx.send(DiscordEvent::Heartbeat) {
+                tx.send(DiscordEvent::DiscordError(String::from(format!("{}", e))))
+                    .unwrap_or_default();
+            }
+
+            if let Err(e) = client.start().await {
+                tx.send(DiscordEvent::DiscordError(String::from(format!("{}", e))))
+                    .unwrap_or_default();
+            }
+        });
+    });
+
+    // heartbeat
+    println!("Waiting for heartbeat message...");
+    if let Err(e) = rx.recv_timeout(Duration::from_secs(5)) {
+        println!("{}", Color::Red.paint(format!("Failed to start: {}", e)));
+        exit(1);
     }
-};
-use iced::{
-    Application, executor, Command, Element, Container,
-    Length, Settings, Column, Text
-};
+
+    // run chord
+    Chord::run(Settings::default())
+}
 
 struct DiscordPipe;
 
@@ -23,47 +64,11 @@ impl prelude::TypeMapKey for DiscordPipe {
     type Value = std::sync::Mutex<mpsc::Sender<DiscordEvent>>;
 }
 
-#[tokio::main]
-pub async fn main() {
-    // IPC
-    let (send, _receive): (mpsc::Sender<DiscordEvent>, mpsc::Receiver<DiscordEvent>) = mpsc::channel();
-    let serene_send = send.clone();
-
-    // Spawn Discord manager
-    let _serene = tokio::spawn(async move {
-        let framework = StandardFramework::new()
-        .group(&GENERAL_GROUP);
-
-        // Login with a bot token from the environment
-        let token = env::var("DISCORD_TOKEN").expect("token");
-        let mut client = Client::new(token)
-            .event_handler(Handler)
-            .framework(framework)
-            .await
-            .expect("Error creating client");
-
-        // Pass down send channel clone to client data.
-        {
-            let mut data = client.data.write().await;
-            data.insert::<DiscordPipe>(std::sync::Mutex::new(serene_send));
-        }
-
-        if let Err(why) = client.start().await {
-            println!("An error occurred while running the client: {:?}", why);
-        }
-    });
-
-    // Run GUI
-    let _gui = tokio::spawn(async move {
-        Chord::run(Settings::default())
-    });
-
-    futures::join!(_serene, _gui);
-}
-
 // General IPC
 enum DiscordEvent {
-    MsgRecv(Message),
+    Heartbeat,
+    MsgRecv(String),
+    DiscordError(String),
 }
 
 // Serene
@@ -75,15 +80,23 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn ready(&self, _: Context, ready: Ready) {
-        println!("Connected to user {}#{}", ready.user.name, ready.user.discriminator);
+        println!(
+            "Connected to user {}#{}",
+            ready.user.name, ready.user.discriminator
+        );
     }
 
     async fn message(&self, ctx: Context, msg: Message) {
-        match ctx.data.read().await.get::<DiscordPipe>()
-        .unwrap()
-        .lock()
-        .unwrap()
-        .send(DiscordEvent::MsgRecv(msg)) {
+        match ctx
+            .data
+            .read()
+            .await
+            .get::<DiscordPipe>()
+            .unwrap()
+            .lock()
+            .unwrap()
+            .send(DiscordEvent::MsgRecv(msg.content))
+        {
             Ok(_) => println!("Sent message through channel"),
             Err(e) => println!("{}", e),
         }
@@ -99,10 +112,7 @@ impl Application for Chord {
     type Flags = ();
 
     fn new(_flags: ()) -> (Self, Command<Self::Message>) {
-        (
-            Chord,
-            Command::none(),
-        )
+        (Chord, Command::none())
     }
 
     fn title(&self) -> String {
@@ -114,12 +124,7 @@ impl Application for Chord {
     }
 
     fn view(&mut self) -> Element<Self::Message> {
-        Container::new(
-            Column::new()
-                .push(
-                    Text::new(String::from("Receiving messages...")),
-                )
-            )
+        Container::new(Column::new().push(Text::new(String::from("Receiving messages..."))))
             .width(Length::Fill)
             .height(Length::Fill)
             .padding(20)

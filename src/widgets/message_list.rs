d5292b96858a5f864cbac2855926be2a3755a153
krashanoff <lkrashanoff2@gmail.com>
2020-09-24 14:31:26 -0700

    Implicit typing for MessageList

--- src/widgets/message_list.rs
+++ src/widgets/message_list.rs
@@ -18,23 +18,18 @@ impl MessageList {
         }
     }
 
-    pub fn update(&mut self, msg: IntMessage) {
-        println!("hello!");
-    }
-
     pub fn view(&mut self) -> Element<IntMessage> {
-        let mut content: Column<Text> = self
+        let content = self
             .messages
             .iter()
             .fold(Column::new().spacing(20), |column, msg| {
                 column.push(Text::new(msg.clone()))
             });
 
-        let mut scrolling = Scrollable::new(&mut self.scroll_state)
+        let scrolling = Scrollable::new(&mut self.scroll_state)
             .padding(20)
             .push(content);
 
-        // TODO: why doesn't this build?
         Container::new(scrolling).into()
     }
 }
